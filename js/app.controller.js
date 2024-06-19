675667c5f3f1de84879050ca36be8af0aff12da6
Elad Berliner <eladberliner@gmail.com>
2024-06-19 16:26:28 +0300

    Oninit-created modal. fixed with deletion prompt, all is ok. crtd by Elad

--- js/app.controller.js
+++ js/app.controller.js
@@ -68,18 +68,9 @@ function renderLocs(locs) {
     document.querySelector('.debug').innerText = JSON.stringify(locs, null, 2)
 }
 
-function onRemoveLoc(locId) {
-    locService.remove(locId)
-        .then(() => {
-            flashMsg('Location removed')
-            unDisplayLoc()
-            loadAndRenderLocs()
-        })
-        .catch(err => {
-            console.error('OOPs:', err)
-            flashMsg('Cannot remove location')
-        })
-}
+
+
+
 
 function onSearchAddress(ev) {
     ev.preventDefault()
@@ -313,4 +304,56 @@ function cleanStats(stats) {
         return acc
     }, [])
     return cleanedStats
+}
+
+
+
+
+let locIdToDelete;
+
+export function onRemoveLoc(locId) {
+    locIdToDelete = locId;
+    showDeleteModal();
+}
+
+// Function to show the delete confirmation modal
+function showDeleteModal() {
+    document.querySelector('.delete-modal').style.display = 'block';
+}
+
+// Function to hide the delete confirmation modal
+function hideDeleteModal() {
+    document.querySelector('.delete-modal').style.display = 'none';
+}
+
+// Event listeners for the modal buttons
+document.querySelector('.xbtn').addEventListener('click', hideDeleteModal);
+document.querySelector('.deletebtn').addEventListener('click', () => {
+    onRemoveLocConfirmed(locIdToDelete);
+    hideDeleteModal();
+});
+
+function flashMsg2(msg) {
+    const el = document.querySelector('.user-msg');
+    el.innerText = msg;
+    el.classList.add('open');
+    setTimeout(() => {
+        el.classList.remove('open');
+    }, 3000);
+}
+
+
+
+
+function onRemoveLocConfirmed(locId) {
+    locService.remove(locId)
+        .then(() => {
+            flashMsg2('Location removed');
+            unDisplayLoc();
+            loadAndRenderLocs();
+        })
+        .catch(err => {
+            console.error('OOPs:', err);
+            flashMsg2('Cannot remove location');
+        });
 }
